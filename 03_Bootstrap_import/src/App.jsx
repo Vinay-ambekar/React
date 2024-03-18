cb9ad3677d03f6ecc844d9573acc690547b1c1c9
vinay <vinayambekar.b@gmail.com>
2024-03-18 11:12:59 +0530

    'bootstrap'

---
+++ 03_Bootstrap_import/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  return <div>
+    <button class='btn btn-danger'>Click Me</button>{/* bootstrap button */}
+  </div>
+  
+  
+   
+}
+
+export default App;
